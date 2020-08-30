d726f2effc62d222e0d61f904573a3402160abff
Plich Justyna-TDN473 <justyna.plich@motorolasolutions.com>
2020-08-30 15:28:44 +0200

    Another try with SPI

--- main_application.py
+++ main_application.py
@@ -9,28 +9,36 @@ from AD_functions import *
 
 class MainFrame(wx.Frame):
     def __init__(self):
-        super().__init__(parent=None, title="ADC TESTER")
-        panel = wx.Panel(self)
+        super().__init__(parent=None, title="ADC TESTER", size=(1500, 1000))
+
+        self.splitter = wx.SplitterWindow(self)
+        self.left_Panel = wx.Panel(self.splitter, style=wx.SUNKEN_BORDER)
+        self.right_Panel = wx.Panel(self.splitter, style=wx.SUNKEN_BORDER)
+
+        self.splitter.SplitVertically(self.left_Panel, self.right_Panel, 500)
+
+        # panel = wx.Panel(self)
 
         my_box_sizer = wx.BoxSizer(wx.VERTICAL)
 
-        btnDeviceSearch = wx.Button(panel, label="Search for device")
+        btnDeviceSearch = wx.Button(self.left_Panel, label="Search for device")
         btnDeviceSearch.Bind(wx.EVT_BUTTON, self.on_press_search)
         my_box_sizer.Add(btnDeviceSearch, 0, wx.ALL | wx.CENTER, 5)
 
-        btnStartMeasurement = wx.Button(panel, label="Start Measurement")
+        btnStartMeasurement = wx.Button(
+            self.left_Panel, label="Start Measurement")
         btnStartMeasurement.Bind(wx.EVT_BUTTON, self.on_press_measurement)
         my_box_sizer.Add(btnStartMeasurement, 0, wx.ALL | wx.CENTER, 5)
 
-        btnExit = wx.Button(panel, label="Exit")
+        btnExit = wx.Button(self.left_Panel, label="Exit")
         btnExit.Bind(wx.EVT_BUTTON, self.on_press_exit)
         my_box_sizer.Add(btnExit, 0, wx.ALL | wx.CENTER, 5)
 
-        #btnSetConfiguration = wx.Button(panel, label="Set Configuration")
-        #btnSetConfiguration.Bind(wx.EVT_BUTTON, self.on_press)
-        #my_box_sizer.Add(btnSetConfiguration, 0, wx.ALL | wx.CENTER, 5)
+        # btnSetConfiguration = wx.Button(panel, label="Set Configuration")
+        # btnSetConfiguration.Bind(wx.EVT_BUTTON, self.on_press)
+        # my_box_sizer.Add(btnSetConfiguration, 0, wx.ALL | wx.CENTER, 5)
 
-        self.text_ctrl = wx.TextCtrl(panel,
+        self.text_ctrl = wx.TextCtrl(self.right_Panel,
                                      size=(400, 200),
                                      style=wx.TE_MULTILINE | wx.TE_READONLY | wx.HSCROLL)
 
@@ -39,28 +47,32 @@ class MainFrame(wx.Frame):
         redir = RedirectText(self.text_ctrl)
         sys.stdout = redir
 
-        panel.SetSizer(my_box_sizer)
+        self.left_Panel.SetSizer(my_box_sizer)
         self.Show()
 
     def on_press_measurement(self, event):
-        #idxDevice, hdwf = dwf_open_first_device_as_analogout()
+        print("On press measurement")
+        # idxDevice, hdwf = dwf_open_first_device_as_analogout()
         # idxDevice, hdwf =
-        dwf_open_first_device_as_analogout()
+        dwf_dio, dwf_aio, dwf_do, dwf_di, dwf_ao = dwf_open_first_device()
+        self.to_plot = spi_send_data(dwf_do, dwf_di, dwf_dio)
 
-        #dwf_configure_spi(hdwf, 10)
+        # dwf_configure_spi(hdwf, 10)
 
-        #waveform_test_channel = 0
-        #waveform_test_frequency = 0.1
-        #waveform_test_amplitude = 1.0
+        waveform_test_channel = 0
+        waveform_test_frequency = 0.1
+        waveform_test_amplitude = 1.0
 
-        # dwf_generate_custom_waveform(idxDevice, waveform_test_channel, waveform_test_frequency,
-        # waveform_test_amplitude, test_waveform_ascending_and_descending)
+        # dwf_generate_custom_waveform(dwf_ao, waveform_test_channel, waveform_test_frequency,
+        #                            waveform_test_amplitude, test_waveform_ascending_and_descending)
 
-        #test_adc(idxDevice, waveform_test_channel)
-        #print("Signal generated for 10 seconds. Started")
+        test_adc(dwf_ao, waveform_test_channel, dwf_dio)
+        # print("Signal generated for 10 seconds. Started")
         # time.sleep(10)
         # print("Done")
         # idxDevice.close()
+
+        dwf_close_device()
 
     def on_press_search(self, event):
         self.plot_results()
@@ -68,15 +80,16 @@ class MainFrame(wx.Frame):
         # dwf_open_first_device_as_analogout()
 
     def on_press_exit(self, event):
-        exit_program(exit_cases.END_OF_PROGRAM)
+        print("On press exit")
+        # exit_program(exit_cases.END_OF_PROGRAM)
 
-    # def print_in_monitor(self):
-    #     wx.Printer(data="Hell
+        # def print_in_monitor(self):
+        #     wx.Printer(data="Hell
     def plot_results(self):
-        x = [*range(0, 4096)]
-        y = [*range(0, 4096)]
+        # x = [*range(0, 4096)]
+        # y = [*range(0, 4096)]
 
-        plt.plot(x, y)
+        plt.plot(self.to_plot)  # x, y)  # self.to_plot)
         plt.show()
 
 
